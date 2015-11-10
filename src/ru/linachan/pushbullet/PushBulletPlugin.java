798471c4ba7a81365887d6378691d1491e8f5717
Alexey Deryugin <aderyugin@mirantis.com>
2015-11-10 20:07:17 +0300

    Project skeleton

---
+++ src/ru/linachan/pushbullet/PushBulletPlugin.java
@@ -0,0 +1,26 @@
+package ru.linachan.pushbullet;
+
+import ru.linachan.yggdrasil.component.YggdrasilPlugin;
+
+public class PushBulletPlugin extends YggdrasilPlugin {
+
+    @Override
+    protected void setUpDependencies() {
+
+    }
+
+    @Override
+    protected void onInit() {
+
+    }
+
+    @Override
+    protected void onShutdown() {
+
+    }
+
+    @Override
+    protected boolean executeTests() {
+        return false;
+    }
+}
